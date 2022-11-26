af65fdc6118e045192f00704723997c775a1671d
FarhanAshari23 <Farhanashari2346@gmail.com>
2022-11-26 14:56:56 +0700

    Fix Late Error and other

--- lib/views/detailTransaction.dart
+++ lib/views/detailTransaction.dart
@@ -25,76 +25,95 @@ class DetailTransactionPage extends StatefulWidget {
 class _DetailTransactionPageState extends State<DetailTransactionPage> {
   @override
   Widget build(BuildContext context) {
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.start,
-      children: [
-        Padding(
-          padding: const EdgeInsets.only(top: 105, left: 120),
-          child: Container(
-            width: 150,
-            height: 150,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              image: DecorationImage(
-                image: AssetImage(widget.imageOne),
-                fit: BoxFit.cover,
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.black,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/cloth.png',
+              height: 25,
+              width: 25,
+            ),
+            const SizedBox(width: 10),
+            const Text("H&M Online Shop"),
+          ],
+        ),
+      ),
+      backgroundColor: const Color(0xff1167b1),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: const EdgeInsets.only(top: 40, left: 120),
+            child: Container(
+              width: 150,
+              height: 150,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                image: DecorationImage(
+                  image: AssetImage(widget.imageOne),
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.only(left: 45, top: 20),
-          child: Container(
-            width: 300,
-            height: 350,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.blueGrey,
-            ),
-            child: Padding(
-              padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  Text(
-                    "code: ${widget.code}",
-                    style: detailProduct,
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    "Name Products: ${widget.name}",
-                    style: detailProduct,
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    "Colour: ${widget.colour}",
-                    style: detailProduct,
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    "Price: ${widget.price}",
-                    style: detailProduct,
-                  ),
-                  const SizedBox(height: 15),
-                  Text(
-                    "No Article: ${widget.status}",
-                    style: detailProduct,
-                  ),
-                ],
+          Padding(
+            padding: const EdgeInsets.only(left: 45, top: 20),
+            child: Container(
+              width: 300,
+              height: 350,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: const Color(0xff03254C),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.only(top: 30, left: 20, right: 20),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "code: ${widget.code}",
+                      style: detailProduct,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "Name Products: ${widget.name}",
+                      style: detailProduct,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "Colour: ${widget.colour}",
+                      style: detailProduct,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "Price: ${widget.price}",
+                      style: detailProduct,
+                    ),
+                    const SizedBox(height: 15),
+                    Text(
+                      "Status: ${widget.status}",
+                      style: detailProduct,
+                    ),
+                  ],
+                ),
               ),
             ),
           ),
-        ),
-        const SizedBox(height: 30),
-        Padding(
-          padding: const EdgeInsets.only(left: 150),
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: const Text("Get Back")),
-        )
-      ],
+          const SizedBox(height: 30),
+          Padding(
+            padding: const EdgeInsets.only(left: 150),
+            child: ElevatedButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                child: const Text("Get Back")),
+          )
+        ],
+      ),
     );
   }
 }
--- lib/views/home.dart
+++ lib/views/home.dart
@@ -23,6 +23,7 @@ class _HomePageState extends State<HomePage> {
   @override
   void initState() {
     super.initState();
+    _clothes = [];
     getClothes();
   }
 
